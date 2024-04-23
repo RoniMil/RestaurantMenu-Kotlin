b1fae3ca9ef6a7264ed6b7ac0a5b4a2cb0dd97f9
Roni Milstein <milsteinroni@gmail.com>
2024-04-23 17:55:21 +0300

    preparing to implement app functionality, added localization strings for menu items

--- app/src/main/java/com/example/midtermdrill/MenuItem.kt
+++ app/src/main/java/com/example/midtermdrill/MenuItem.kt
@@ -2,6 +2,6 @@ package com.example.midtermdrill
 
 // This class will define the menu item object
 data class MenuItem(
-    val id: Int, val name: String, val price: Double, val description: String, val imageUrl: String
+    val name: String, val price: Double, val description: String, val imageUrl: String
 )
 
