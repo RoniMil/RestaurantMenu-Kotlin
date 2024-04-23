1370239c88651a386c30bce2e7fc7c72d1690a90
Roni Milstein <milsteinroni@gmail.com>
2024-04-23 13:23:51 +0300

    fixed buttons not appearing in different devices, added buttons for food and drink menus, started work on app functionality

---
+++ app/src/main/java/com/example/midtermdrill/MenuAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.midtermdrill
+
+class MenuAdapter {
+}
---
+++ app/src/main/java/com/example/midtermdrill/MenuItem.kt
@@ -0,0 +1,7 @@
+package com.example.midtermdrill
+
+// This class will define the menu item object
+data class MenuItem(
+    val id: Int, val name: String, val price: Double, val description: String, val imageUrl: String
+)
+
